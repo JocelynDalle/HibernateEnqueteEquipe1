36030f56043a1fda2ed0d585818bd88fafafb8aa
EricEDX <yodenai-git@yahoo.fr>
2017-02-17 10:46:30 +0100

    implémentation des classes Department et Fact

--- src/main/java/fr/humanbooster/fx/enquetes/business/Department.java
+++ src/main/java/fr/humanbooster/fx/enquetes/business/Department.java
@@ -1,5 +1,53 @@
 package fr.humanbooster.fx.enquetes.business;
 
-public class Department {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import org.hibernate.validator.constraints.Length;
+
+public class Department {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Length(max=3)
+	private String number;
+	
+	@Length(max=50)
+	private String name;
+	
+	public Department(String number, String name) {
+		this.number = number;
+		this.name = name;
+	}
+	
+	public Department() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
--- src/main/java/fr/humanbooster/fx/enquetes/business/Fact.java
+++ src/main/java/fr/humanbooster/fx/enquetes/business/Fact.java
@@ -1,5 +1,28 @@
 package fr.humanbooster.fx.enquetes.business;
 
-public class Fact {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Fact {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String wording;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getWording() {
+		return wording;
+	}
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
+	
+	
 }
