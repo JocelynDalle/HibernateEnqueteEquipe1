3abe9ab5a24d27f32e5d8fd85d715a0bd88ebbc8
DJoce <dalle.jocelyn@gmail.com>
2017-02-17 15:35:36 +0100

    finalisation script.sql

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -28,5 +28,31 @@ INSERT INTO survey (DTYPE, date, name, price, script) VALUES
 ('SurveyPhone','2016-09-22', "Enquête sur les skis dans le Puy-De-Dôme", 3800, "Bonjour, aimez-vous les skis ???"),
 ('SurveyPhone','2017-01-09', "Enquête sur les Voyages vers Barcelone", 3300, "Bonjour, aimez-vous les avions ???");
 
+INSERT INTO survey (DTYPE, date, name, price) VALUES
+('SurveyInternet','2015-10-05', "Enquête sur les soupes dans la Loire", 750), 
+('SurveyInternet','2016-09-25', "Enquête sur les skis dans le Puy-De-Dôme", 1900),
+('SurveyInternet','2017-01-14', "Enquête sur les Voyages vers Barcelone", 1700);
+
+INSERT INTO survey_partnersite VALUES
+(4, 1),
+(4, 2),
+(5, 3),
+(6, 1),
+(6, 3),
+(6, 4);
+
+INSERT INTO question (wording, survey_id) VALUES 
+("Que pensez-vous des soupes alphabet en général ?", 1),
+("Que pensez-vous des soupes alphabet en général ?", 4),
+("Préférez-vous les pâtes ou les légumes ?", 1),
+("Pratiquez-vous le ski régulièrement ?", 2),
+("Êtes-vous déjà allé à Barcelone ?", 3);
+
+INSERT INTO criteria (quota, department_id, fact_id, survey_id) VALUES 
+(100, 7, 2, 1),
+(250, 9, 3, 2),
+(200, 1, 1, 3);
+
+
 
 
