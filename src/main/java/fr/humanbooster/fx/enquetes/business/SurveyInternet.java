3ed664f1842aebcaef7ec722e114d83b53add90d
DJoce <dalle.jocelyn@gmail.com>
2017-02-17 12:13:40 +0100

    modifications sur hibernate.cfg.xml + miror fix

--- src/main/java/fr/humanbooster/fx/enquetes/business/SurveyInternet.java
+++ src/main/java/fr/humanbooster/fx/enquetes/business/SurveyInternet.java
@@ -2,21 +2,25 @@ package fr.humanbooster.fx.enquetes.business;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 
+@Entity
 public class SurveyInternet extends Survey {
 	
-	@ManyToMany (fetch = FetchType.EAGER)
-	private Set<PartnerSite> setPartnerSite;
+	@ManyToMany(fetch=FetchType.EAGER)
+	private Set<PartnerSite> lsPartnerSite;
 
-	public Set<PartnerSite> getSetPartnerSite() {
-		return setPartnerSite;
+	public Set<PartnerSite> getLsPartnerSite() {
+		return lsPartnerSite;
 	}
 
-	public void setSetPartnerSite(Set<PartnerSite> setPartnerSite) {
-		this.setPartnerSite = setPartnerSite;
+	public void setLsPartnerSite(Set<PartnerSite> lsPartnerSite) {
+		this.lsPartnerSite = lsPartnerSite;
 	}
+
+
 	
 	
 
