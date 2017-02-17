c8f8bee7cefcc17eb56a99a40e679d5549f8abb2
llbb <lboulanger@humanbooster.com>
2017-02-17 11:06:08 +0100

    classes business question et criteria

--- src/main/java/fr/humanbooster/fx/enquetes/business/Question.java
+++ src/main/java/fr/humanbooster/fx/enquetes/business/Question.java
@@ -1,5 +1,50 @@
 package fr.humanbooster.fx.enquetes.business;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Question {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String wording;
+	
+	@ManyToOne
+	private Survey survey;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getWording() {
+		return wording;
+	}
+
+	public void setWording(String wording) {
+		this.wording = wording;
+	}
+
+	public Survey getSurvey() {
+		return survey;
+	}
+
+	public void setSurvey(Survey survey) {
+		this.survey = survey;
+	}
+
+	@Override
+	public String toString() {
+		return "Question [id=" + id + ", wording=" + wording + ", survey=" + survey + "]";
+	}
 
 }
