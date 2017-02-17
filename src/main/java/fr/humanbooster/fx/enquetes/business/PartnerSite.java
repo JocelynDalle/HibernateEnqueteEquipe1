40e3286de36312c1615404b71ca83361f8e9686d
EricEDX <yodenai-git@yahoo.fr>
2017-02-17 11:12:02 +0100

    classe PartnerSite implémentée

--- src/main/java/fr/humanbooster/fx/enquetes/business/PartnerSite.java
+++ src/main/java/fr/humanbooster/fx/enquetes/business/PartnerSite.java
@@ -1,5 +1,46 @@
 package fr.humanbooster.fx.enquetes.business;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class PartnerSite {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String url;
+	
+	@ManyToMany(mappedBy="lsPartnerSite", fetch=FetchType.EAGER)
+	Set<SurveyInternet> lsSurveyInternet;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
 }
