abab5be48c7fea05b0aaa26f5f85ad2780d8cc1f
DJoce <dalle.jocelyn@gmail.com>
2017-02-17 19:48:47 +0100

    ajout de la JSP modifieQuestion et modification des Servlets associées

--- src/main/java/fr/humanbooster/fx/enquetes/service/impl/QuestionServiceImpl.java
+++ src/main/java/fr/humanbooster/fx/enquetes/service/impl/QuestionServiceImpl.java
@@ -8,14 +8,16 @@ import fr.humanbooster.fx.enquetes.dao.impl.QuestionDaoImpl;
 import fr.humanbooster.fx.enquetes.dao.impl.SurveyDaoImpl;
 import fr.humanbooster.fx.enquetes.service.QuestionService;
 
-public class QuestionServiceImpl implements QuestionService{
+public class QuestionServiceImpl implements QuestionService {
 
 	private QuestionDao qDao = new QuestionDaoImpl();
 	private SurveyDao sDao = new SurveyDaoImpl();
-	
+
 	@Override
-	public Question modifyQuestion(Question question) {
+	public Question modifyQuestion(String idQuestion, String wording) {
 		qDao.openCurrentSessionWithTransaction();
+		Question question = qDao.findById(Integer.parseInt(idQuestion));
+		question.setWording(wording);
 		Question q = qDao.updateQuestion(question);
 		qDao.closeCurrentSessionwithTransaction();
 		return q;
@@ -24,27 +26,36 @@ public class QuestionServiceImpl implements QuestionService{
 	@Override
 	public Boolean deleteQuestion(String idQuestion) {
 		qDao.openCurrentSessionWithTransaction();
-		Boolean b = qDao.deleteQuestion(Integer.parseInt(idQuestion));
+		boolean b = qDao.deleteQuestion(Integer.parseInt(idQuestion));
 		qDao.closeCurrentSessionwithTransaction();
 		return b;
 	}
 
 	@Override
 	public Question addQuestionToSurvey(String wording, String idSurvey) {
-		
+
 		sDao.openCurrentSessionWithTransaction();
 		Survey survey = sDao.findById(Integer.parseInt(idSurvey));
 		sDao.closeCurrentSessionwithTransaction();
-		
+
 		Question question = new Question();
 		question.setWording(wording);
 		question.setSurvey(survey);
-		
+
 		qDao.openCurrentSessionWithTransaction();
 		qDao.createQuestion(question);
 		qDao.closeCurrentSessionwithTransaction();
-		
+
 		return question;
+	}
+
+	@Override
+	public Question findQuestionById(String idQuestion) {
+		int idQuestionInt = Integer.parseInt(idQuestion);
+		qDao.openCurrentSessionWithTransaction();
+		Question q = qDao.findById(idQuestionInt);
+		qDao.closeCurrentSessionwithTransaction();
+		return q;
 	}
 
 }
