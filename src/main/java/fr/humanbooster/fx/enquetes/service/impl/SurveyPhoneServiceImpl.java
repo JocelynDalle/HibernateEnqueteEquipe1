0b027bdb8f2375fe4d9aa49cfd302abe1bff2c24
Jorbyland <gorny.robin@gmail.com>
2017-02-17 15:06:31 +0100

    surveys internet and phone interfaces

---
+++ src/main/java/fr/humanbooster/fx/enquetes/service/impl/SurveyPhoneServiceImpl.java
@@ -0,0 +1,14 @@
+package fr.humanbooster.fx.enquetes.service.impl;
+
+import fr.humanbooster.fx.enquetes.business.SurveyPhone;
+import fr.humanbooster.fx.enquetes.service.SurveyPhoneService;
+
+public class SurveyPhoneServiceImpl implements SurveyPhoneService{
+
+	@Override
+	public SurveyPhone createSurveyPhone(SurveyPhone surveyPhone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
